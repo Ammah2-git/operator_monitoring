0d335e0e0f4575d385c57118624f39ac72e05771
Ammah2-git <ahmed.a.assagaf@gmail.com>
2024-12-04 12:09:07 +0800

    update

---
+++ models/train.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+import os
+from roboflow import Roboflow
+
+# rf = Roboflow(api_key="")
+# project = rf.workspace("").project("")
+# version = project.version(1)
+# dataset = version.download("")
+
+# model = YOLO("")
+
+# results = model.train(data=".yaml", epochs=1000)
